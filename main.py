34e5106deec0ee43053ce5ec9cc6e8f46d9d891e
Daniel Roberts <29379642+danielroberts20@users.noreply.github.com>
2026-02-16 18:31:15 +0000

    Updated currencies

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 ACCESS_KEY = os.environ["FX_API_KEY"]
 UPLOAD_TOKEN = os.environ["UPLOAD_TOKEN"]
-CURRENCIES = ["GBP", "USD", "CAN", "AUD", "NZD"]
+CURRENCIES = ["GBP", "USD", "CAN", "AUD", "NZD", "FJD", "THB", "KHR", "VND", "LAK"]
 SOURCE = "GBP"
 API_URL = "http://pi-server:8000"
 FX_URL = "https://api.exchangerate.host/historical"
