89070122ed7e5d37f046765a84130a3d68774cf5
Daniel Roberts <29379642+danielroberts20@users.noreply.github.com>
2026-02-16 19:09:37 +0000

    Made dedicated finance module for FX related functions

---
+++ finance.py
@@ -0,0 +1,31 @@
+import calendar
+import time
+from datetime import datetime
+
+import requests
+
+from config import ACCESS_KEY, FX_URL
+from server import upload_fx
+
+
+def get_fx_rate_at_date(date_string, *currencies, **kwargs):
+    try:
+        datetime.strptime(date_string, "%Y-%m-%d")
+        params = {
+            "access_key": ACCESS_KEY,
+            "date": date_string,
+            "source": kwargs.get("source") or "GBP",
+            "currencies": ",".join(list(currencies)),
+        }
+        response = requests.get(FX_URL, params=params)
+        return response.json()
+    except ValueError:
+        return None
+
+def get_fx_for_month(month=None, year=None):
+    now = datetime.now()
+    month = now.month - 1 if month is None else month
+    year = now.year if year is None else year
+    for day in range(1, calendar.monthrange(year, month)[1]+1):
+        upload_fx(get_fx_rate_at_date(f"{year}-{month:02}-{day:02}", *CURRENCIES, source=SOURCE))
+        time.sleep(2.5)
