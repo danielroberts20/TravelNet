b0ba87a0b472655de4288720079617907adfcdd3
Daniel Roberts <29379642+danielroberts20@users.noreply.github.com>
2026-02-23 17:18:56 +0000

    Skeleton code for logging, local DB download, ML pipeline

--- client/main.py
+++ client/main.py
@@ -1,3 +1,16 @@
-from server_interaction import get_recent_locations
+import cleaning
+import enrichment
+import feature_engineering
+from client.logging_config import configure_logging
+from server_interaction import download_database
 
-get_recent_locations(14) # Get last 2 weeks of location data
+def start_client():
+    configure_logging()
+    download_database()
+
+cleaned_data = cleaning.dummy_func(None)
+enriched_data = enrichment.dummy_func(cleaned_data)
+c = feature_engineering.dummy_func(enriched_data)
+
+if __name__ == "__main__":
+    start_client()
