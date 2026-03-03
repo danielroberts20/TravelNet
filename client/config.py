71b2d6299b003f79b87873bd6755da81ab6c6136
Daniel Roberts <29379642+danielroberts20@users.noreply.github.com>
2026-03-03 18:41:06 +0000

    Moved server URL to env

--- client/config.py
+++ client/config.py
@@ -11,6 +11,6 @@ ACCESS_KEY = os.environ["FX_API_KEY"]
 UPLOAD_TOKEN = os.environ["UPLOAD_TOKEN"]
 CURRENCIES = ["GBP", "USD", "CAN", "AUD", "NZD", "FJD", "THB", "KHR", "VND", "LAK"]
 SOURCE = "GBP"
-SERVER_URL = URL("http://pi-server.tail186ff8.ts.net:8000")
+SERVER_URL = URL(os.environ["SERVER_URL"])
 FX_URL = URL("https://api.exchangerate.host/historical")
 DATA_DIR = Path("data")
