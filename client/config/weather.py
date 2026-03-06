b0297058782f0b6a5507e4885792cc8fe3f4f106
Daniel Roberts <29379642+danielroberts20@users.noreply.github.com>
2026-03-06 18:06:48 +0000

    Added initial weather enrichment files

---
+++ client/config/weather.py
@@ -0,0 +1,10 @@
+WEATHER_METRICS = [
+    "temperature_2m",
+    "precipitation",
+    "weathercode",
+    "windspeed_10m",
+    "apparent_temperature",
+    "uv_index",
+    "sunshine_duration",
+    "relative_humidity_2m"
+]
