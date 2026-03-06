aca083dbc5287426d1f30da5d067655ce1b5595c
Daniel Roberts <29379642+danielroberts20@users.noreply.github.com>
2026-03-06 17:48:23 +0000

    Added endpoints for uploading Wise and Revolut transactions

--- server/app/uploads/endpoints.py
+++ server/app/uploads/endpoints.py
@@ -1,3 +1,4 @@
+import csv
 from datetime import datetime
 import io
 from fastapi import APIRouter, Header, BackgroundTasks, UploadFile, File, HTTPException  #type: ignore
@@ -5,7 +6,9 @@ import logging
 from typing import Any
 
 from auth import check_auth
-from config.general import HEALTH_BACKUP_DIR, LOCATION_BACKUP_DIR
+from config.general import HEALTH_BACKUP_DIR, LOCATION_BACKUP_DIR, REVOLUT_TRANSACTION_BACKUP_DIR, WISE_TRANSACTION_BACKUP_DIR
+from database.transaction.ingest.wise import insert as insert_wise
+from database.transaction.ingest.revolut import insert as insert_revolut
 from uploads.utils import handle_health_upload, input_csv
 
 router = APIRouter()
@@ -61,4 +64,63 @@ async def upload_csv(file: UploadFile = File(...),
         "status": "success",
         "rows_inserted": inserted,
         "skipped_rows": skipped_rows
+    }
+
+@router.post("/wise")
+async def upload_wise(file: UploadFile = File(...),
+                      authorization: str = Header(None),
+                      source: str = Header(None)):
+    check_auth(authorization)
+
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(status_code=400, detail="File must be a CSV")
+
+    print(source)
+    
+    contents = await file.read()
+
+    # Decode bytes → string
+    decoded = contents.decode("utf-8")
+    now = datetime.now()
+    year_month = now.strftime("%Y-%m")
+    day = int(now.strftime("%d"))-1
+    csv_path = WISE_TRANSACTION_BACKUP_DIR / f"{year_month}-{day}.csv"
+    with open(csv_path, "w+") as f:
+        f.write(decoded)
+        f.close()
+
+    inserted, skipped, errors = insert_wise(csv_path, source)
+
+    return {
+        "inserted": inserted,
+        "skipped": skipped,
+        "errors": errors
+    }
+
+@router.post("/revolut")
+async def upload_revolut(file: UploadFile = File(...),
+                      authorization: str = Header(None)):
+    check_auth(authorization)
+
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(status_code=400, detail="File must be a CSV")
+    
+    contents = await file.read()
+
+    # Decode bytes → string
+    decoded = contents.decode("utf-8")
+    now = datetime.now()
+    year_month = now.strftime("%Y-%m")
+    day = int(now.strftime("%d"))-1
+    csv_path = REVOLUT_TRANSACTION_BACKUP_DIR / f"{year_month}-{day}.csv"
+    with open(csv_path, "w+") as f:
+        f.write(decoded)
+        f.close()
+
+    inserted, skipped, errors = insert_revolut(csv_path)
+
+    return {
+        "inserted": inserted,
+        "skipped": skipped,
+        "errors": errors
     }
