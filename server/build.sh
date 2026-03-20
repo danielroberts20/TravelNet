24466f69af41bc14c067262353c1d89297d50c91
Daniel Roberts <29379642+danielroberts20@users.noreply.github.com>
2026-03-20 16:19:50 +0000

    Added no-cache flag for rebuilding containers

--- server/build.sh
+++ server/build.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+CACHE_FLAG=""
+if [[ "$1" == "--no-cache" ]]; then
+  CACHE_FLAG="--no-cache"
+fi
+
 sudo docker stop travelnet travelnet-dashboard
 docker rm travelnet travelnet-dashboard
-docker compose build
+docker compose build $CACHE_FLAG
 docker compose up -d
