2299e7041fd4c0a3e9ee1221225ecbc23481b270
Daniel Roberts <29379642+danielroberts20@users.noreply.github.com>
2026-03-22 15:34:29 +0000

    Ignored build.sh

--- server/build.sh
+++ server/build.sh
@@ -5,7 +5,7 @@ if [[ "$1" == "--no-cache" ]]; then
   CACHE_FLAG="--no-cache"
 fi
 
-sudo docker stop travelnet travelnet-dashboard
-docker rm travelnet travelnet-dashboard
+sudo docker stop travelnet travelnet-dashboard travelnet-nginx
+docker rm travelnet travelnet-dashboard travelnet-nginx
 docker compose build $CACHE_FLAG
 docker compose up -d
